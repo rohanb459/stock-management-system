f76ad70e914c1c5dd66ef859ae16110b15f2783a
ROHAN BHATIA <rohanb459@gmail.com>
2023-10-21 12:36:18 +0530

    initital comit

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// connnect to mongo Db 
+mongoose.connect()
